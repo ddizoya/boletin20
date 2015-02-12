f6aac0ca705ebe4782b61bba00130bf8f0b18ead
ddizoya <ddizoya@eniac30.danielcastelao.org>
2015-02-12 10:54:29 +0100

    Boletín 20.

---
+++ src/boletin20/Boletin20.java
@@ -0,0 +1,25 @@
+/*
+ *Suponer que desde secretaria nos piden un programa para gestionar una lista de alumnos.
+  Cada alumno va a ser identificado por un número de registro. Así mismo, cada alumno tiene
+  un nombre y una nota. Haz un menú con las siguientes opciones:
+        - Dar de alta un alumno.
+        - Dar de baja un alumno.
+        - Localizar un alumno.
+        - 
+ */
+package boletin20;
+
+/**
+ *
+ * @author ddizoya
+ */
+public class Boletin20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
